1a5c52554fb72a0fdadfaedf75a5402f2903ae03
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-05-24 21:22:24 +0530

    fix: added footer and Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navbar from './ components/navbar';
+import { Outlet } from 'react-router-dom';
+import Footer from './ components/footer';
 
 const App = () => {
   return (
     <div data-theme="light">
       <Navbar title={'tinderDev'} />
+      <Outlet />
+      <Footer />
     </div>
   );
 };
