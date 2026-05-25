b013be8272f86359ee35c20214e54c45610d0bf1
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-05-25 14:49:13 +0530

    fix: updating constans

---
+++ src/constant/homepage.js
@@ -0,0 +1,26 @@
+export const cardsDetails = [
+  {
+    id: 'slide1',
+    prev: 'slide4',
+    next: 'slide2',
+    img: 'https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.webp',
+  },
+  {
+    id: 'slide2',
+    prev: 'slide1',
+    next: 'slide3',
+    img: 'https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.webp',
+  },
+  {
+    id: 'slide3',
+    prev: 'slide2',
+    next: 'slide4',
+    img: 'https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.webp',
+  },
+  {
+    id: 'slide4',
+    prev: 'slide3',
+    next: 'slide1',
+    img: 'https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.webp',
+  },
+];
---
+++ src/constant/landingpage.js
@@ -0,0 +1,2 @@
+export const landingPageImage = 'https://tinder.com/static/build/8ad4e4299ef5e377d2ef00ba5c94c44c.webp';
+export const landingPageMobileImage = 'https://tinder.com/static/build/853c9decf9752fa3ae9654095216b391.webp';
